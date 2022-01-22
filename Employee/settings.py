c24aafbcbc1bccbade117ddd29bae6fa929bc6e4
prakashlawagun <prakashlawagun11@gmail.com>
2022-01-22 20:30:39 +0545

    rearrange word

--- Employee/settings.py
+++ Employee/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4@$8on2nbs_1p2th11p95#5zrem@-zdi#o$)a^jgp1p)$yoj5$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
